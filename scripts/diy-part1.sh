3ddeaf5277d32e1968d9b3540c5397beee1bf855
wowoniulcx <70791223+wowoniulcx@users.noreply.github.com>
2021-03-06 08:57:28 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -32,6 +32,23 @@ git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app
 #添加luci-app-vssr   #kenzok8整合包中已有
 #git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 #git clone https://github.com/jerrykuku/luci-app-vssr.git package/vssr
+# DownLoad OpenClash
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+cd -
+# integration clash core 实现编译更新后直接可用，不用手动下载clash内核
+curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p package/luci-app-openclash/luci-app-openclash/files/etc/openclash/core
+mv /tmp/clash package/luci-app-openclash/luci-app-openclash/files/etc/openclash/core/clash >/dev/null 2>&1
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 
 #更新feeds
 ./scripts/feeds update -a
